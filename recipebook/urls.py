d39ae7cb035264661e9cc553e03a91196f4971fc
ennybor11 <ennybor11@gmail.com>
2025-02-24 01:42:49 +0800

    Initialized recipebook project. Note that env is still included.

---
+++ recipebook/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for recipebook project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from ledger import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('recipe/1', views.recipe_1, name='recipe_1'),
+    path('recipes/list', views.recipe_list, name='recipe_list'),
+    path('recipe/2', views.recipe_2, name='recipe_2'),
+]
