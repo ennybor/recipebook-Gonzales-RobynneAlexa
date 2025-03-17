e0fbd31013c71b5e22e759234f9031939d0aa8fe
ennybor11 <ennybor11@gmail.com>
2025-03-17 13:00:32 +0800

    Fixed migration db errors

--- ledger/models.py
+++ ledger/models.py
@@ -14,7 +14,7 @@ which might be what the internet may tell you)
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-# from django.utils import timezone
+#from django.utils import timezone
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
