119d52e5ad3a03a99c82fec85e2fa020bcfa7d36
ennybor11 <ennybor11@gmail.com>
2025-03-17 11:54:27 +0800

    Updated for login user restriction

--- ledger/views.py
+++ ledger/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Recipe
+from django.contrib.auth.decorators import login_required
 
 # Home View
 def home(request):
     return HttpResponse("<h1>Welcome to the Recipe Book!</h1><p><a href='/recipes/list'>View Recipes</a></p>")
 
-
+@login_required(login_url="login") #redirects unauthenticated users to login
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, "ledger/recipe_list.html", {"recipes": recipes})
 
+@login_required(login_url="login")
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
     return render(request, "ledger/recipe_detail.html", {"recipe": recipe})
