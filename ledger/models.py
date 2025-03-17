0d68ece0d957d9a5a5d107d856fd167a6140a204
ennybor11 <ennybor11@gmail.com>
2025-03-17 11:25:53 +0800

    Added profile model and modified reciped model.

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,28 @@
+'''
+Create a Profile model by extending the default User model, which has the following:
+Name - at most 50 characters
+Bio - at most 255 character
+
+Modify the recipe model to contain the following:
+an Author field
+a CreatedOn field that is automatically added upon creation of the instance
+an UpdatedOn field that is automatically updated 
+(Note: You don't have to modify the save function, 
+which might be what the internet may tell you)
+'''
+
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+# from django.utils import timezone
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name or self.user.username 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -9,6 +32,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
