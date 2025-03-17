413c01118c590f6f108117a175e73cba29e4b89f
ennybor11 <ennybor11@gmail.com>
2025-03-17 12:09:12 +0800

    Added login and logout

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 #from . import views
-from .views import home, recipe_list, recipe_detail
+from .views import recipe_list, recipe_detail, login_view, logout_view
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", login_view, name="login"),
     path("recipes/list/", recipe_list, name='recipe_list'),
-    path('recipes/<int:recipe_id>/', recipe_detail, name='recipe_detail'),
+    path("recipes/<int:recipe_id>/", recipe_detail, name='recipe_detail'),
+    path("logout/", logout_view, name="logout"),
 ]
